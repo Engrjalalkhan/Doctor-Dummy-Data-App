ff3d8a4ae3168e751f6d6e75265afab5cec50e34
Jalal khan <jk9494173@gmail.com>
2024-12-18 16:29:39 +0500

    commit

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -73,7 +73,10 @@ const MainScreen = () => {
 
   return (
     <ScrollView>
-      <KeyboardAvoidingView style={styles.container} behavior="height" enabled={false}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="height"
+        enabled={false}>
         <View style={{backgroundColor: '#DF4B38', height: 250}}>
           <View style={styles.Header}>
             <TouchableOpacity>
